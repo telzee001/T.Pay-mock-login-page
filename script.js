992363124546b5f6cd664b067250b930854f31fb
telzee001 <telzee00@gmail.com>
2026-09-09 05:56:24 +0100

    Updated html, css and js

---
+++ script.js
@@ -0,0 +1,4 @@
+const loginBox = document.getElementById('bitcrew-login-box');
+const loginForm = document.getElementById('bitcrew-login-form');
+const bitcrewDashboard = document.getElementById('bitcrew-dashboard');
+const logoutButton = document.getElementById('logout-btn');
